d9a9a31bb4f05c2337d2b9c2a04598193dfe3d5d
Domia <39hson@gmail.com>
2022-01-10 02:00:52 +0700

    add "10. React.createElement() nữa là sao?"

--- MainReact/script.js
+++ MainReact/script.js
@@ -3,13 +3,21 @@ const root = document.getElementById('root');
 const divDOM = document.createElement('div');
 divDOM.className = 'heading';
 const pDOM = document.createElement('p');
-pDOM.innerHTML= 'Hello guys';
+pDOM.innerHTML = 'Hello guys';
 divDOM.appendChild(pDOM);
 root.appendChild(divDOM);
 // React
-const h1React = React.createElement('h1',{
+const h1React = React.createElement('h1', {
   title: 'Hello',
   className: 'heading'
-}, 'Hello guys react'
+}, 'Hello guys react');
+
+const divReact = React.createElement('div', {
+  className: 'post-item'
+}, React.createElement('h2', {
+  title: 'Learn react basic at F8'
+}, 'Learn reactjs'), 
+React.createElement('p', null, 'Learn reactjs from basic to advanced')
 );
 
+console.log(divReact);
